0cea7de81b19e791fbc4e0f0befd3618d9a542de
Yann Moyart <yann.moyart@gmail.com>
2016-10-21 16:32:50 +0200

    recherche de nom dans le fichier crypté

--- credit_card/proc.h
+++ credit_card/proc.h
@@ -11,9 +11,10 @@
 
 #define K_LENGHT 256
 
-
-
+void decrypt_data(FILE *ifp,FILE *ofp,char* ckey);
+void encrypt_data(FILE *ifp,FILE *ofp,char* ckey);
 void decrypt(FILE *ifp, FILE *ofp,char* ckey);
+void decrypt_search(FILE *ifp, FILE *ofp,char* ckey,char* n);
 void encrypt(FILE *ifp, FILE *ofp,char* ckey);
 int simpleSHA256(void* input, unsigned long length, unsigned char* md);
 
